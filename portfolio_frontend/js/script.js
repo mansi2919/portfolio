b02be6f26cdcd4967f819c9b4cd061a5b799093c
mansi2919 <41909232+mansi2919@users.noreply.github.com>
2021-04-20 20:19:31 +0530

    Delete portfolio_frontend directory
    Well I have uploaded the wrong files, so I am deleting them.

--- portfolio_frontend/js/script.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-    $(".btn-output").click(function(){
-        var txt = $(".text").val(); 
-        var sty = $(".sty-css").val();
-        $("style").append(sty);
-        $(".output").html(txt);
-    });
-});
